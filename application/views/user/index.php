0589264f51f02579d17fbc9321b6a7319cdd0b62
muhammad qasim <muhammedqasim902@gmail.com>
2017-10-28 21:04:38 +0500

    Changed multiple forms admin (Qasim)

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -11,25 +11,25 @@
                 <table class="table table-striped">
                     <tr>
 						<th>Userid</th>
-						<th>Password</th>
 						<th>Username</th>
 						<th>Email</th>
+						<th>Password</th>
 						<th>Usertype</th>
-						<th>Created On</th>
+<!--						<th>Created On</th>-->
 						<th>Payment Details</th>
-						<th>Myfavorites</th>
+<!--						<th>Myfavorites</th>-->
 						<th>Actions</th>
                     </tr>
                     <?php foreach($user as $u){ ?>
                     <tr>
 						<td><?php echo $u['userid']; ?></td>
-						<td><?php echo $u['password']; ?></td>
 						<td><?php echo $u['username']; ?></td>
 						<td><?php echo $u['email']; ?></td>
+						<td><?php echo $u['password']; ?></td>
 						<td><?php echo $u['usertype']; ?></td>
-						<td><?php echo $u['created_on']; ?></td>
+<!--						<td>--><?php //echo $u['created_on']; ?><!--</td>-->
 						<td><?php echo $u['payment_details']; ?></td>
-						<td><?php echo $u['myfavorites']; ?></td>
+<!--						<td>--><?php //echo $u['myfavorites']; ?><!--</td>-->
 						<td>
                             <a href="<?php echo site_url('user/edit/'.$u['userid']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
                             <a href="<?php echo site_url('user/remove/'.$u['userid']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Delete</a>
