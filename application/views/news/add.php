3f0049aa54137557c1e9c2d491671b80d9d73b14
rizwanayubi <rizwan.ayubi296@gmail.com>
2017-10-29 00:06:50 +0500

    news add page

--- application/views/news/add.php
+++ application/views/news/add.php
@@ -1,76 +1,124 @@
 <div class="row">
-    <div class="col-md-12">
-      	<div class="box box-info">
-            <div class="box-header with-border">
-              	<h3 class="box-title">News Add</h3>
-            </div>
-            <?php echo form_open('news/add'); ?>
-          	<div class="box-body">
-          		<div class="row clearfix">
-					<div class="col-md-6">
-						<label for="title" class="control-label"><span class="text-danger">*</span>Title</label>
-						<div class="form-group">
-							<input type="text" name="title" value="<?php echo $this->input->post('title'); ?>" class="form-control" id="title" />
-							<span class="text-danger"><?php echo form_error('title');?></span>
+	<div class="col-md-12">
+		<div class="box box-info">
+			<div class="box-header with-border">
+				<h3 class="box-title">Add News</h3>
+			</div>
+			<?php echo form_open('news/add'); ?>
+			<div class="box-body">
+
+				<div class="col-sm-1 col-md-2"></div>
+				<div class="col-sm-10 col-md-8">
+					<div class="callus clearfix border_radius submit_property">
+						<div class="row clearfix">
+							<div class="col-md-6">
+								<label for="title" class="control-label">
+									<span class="text-danger">*</span>Title</label>
+								<div class="form-group">
+									<input type="text" name="title" value="<?php echo $this->input->post('title'); ?>" class="form-control" id="title" />
+									<span class="text-danger">
+										<?php echo form_error('title');?>
+									</span>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<label for="author" class="control-label">
+									<span class="text-danger">*</span>Author</label>
+								<div class="form-group">
+									<input type="text" name="author" value="<?php echo $this->input->post('author'); ?>" class="form-control" id="author" />
+									<span class="text-danger">
+										<?php echo form_error('author');?>
+									</span>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<label for="added_on" class="control-label">
+									<span class="text-danger">*</span>Added On</label>
+									<div class="form-group">
+											<!-- DANISH isko carbon->now sy time pick krwao -->
+										<input type="text" name="added_on" placeholder="Date should be automatically picked" value="<?php echo $this->input->post('added_on'); ?>" class="has-datetimepicker form-control"  id="added_on"  />
+										
+										<span class="text-danger">
+											<?php echo form_error('added_on');?>
+										</span>
+									</div>
+							</div>
+							
+							<!--					<div class="col-md-6">-->
+							<!--						<label for="comment_id" class="control-label"><span class="text-danger">*</span>Comment Id</label>-->
+							<!--						<div class="form-group">-->
+							<!--							<input type="text" name="comment_id" value="-->
+							<?php //echo $this->input->post('comment_id'); ?>
+							<!--" class="form-control" id="comment_id" />-->
+							<!--							<span class="text-danger">-->
+							<?php //echo form_error('comment_id');?>
+							<!--</span>-->
+							<!--						</div>-->
+							<!--					</div>-->
+							<!--					<div class="col-md-6">-->
+							<!--						<label for="excerpt" class="control-label"><span class="text-danger">*</span>Excerpt</label>-->
+							<!--						<div class="form-group">-->
+							<!--							<textarea name="excerpt" class="form-control" id="excerpt">-->
+							<?php //echo $this->input->post('excerpt'); ?>
+							<!--</textarea>-->
+							<!--							<span class="text-danger">-->
+							<?php //echo form_error('excerpt');?>
+							<!--</span>-->
+							<!--						</div>-->
+							<!--					</div>-->
+							<div class="col-md-6">
+								<label for="img_path" class="control-label">
+									<span class="text-danger">*</span> Upload Image</label>
+								<div class="form-group">
+									<div class="input-group agncy_btn">
+										<label class="input-group-btn">
+											<span class="btn btn-primary">
+												Upload Image&hellip; <input type="file" name="img_path" value="" class="form-control" id="img_path" style="display: none;" />
+											</span>
+										</label>
+										<input name="img_path" type="text" value="" class="form-control"  readonly>
+									</div>
+									<span class="text-danger">
+										<?php echo form_error('img_path');?>
+									</span>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<label for="detail" class="control-label">
+									<span class="text-danger">*</span>Detail</label>
+								<div class="form-group">
+									<textarea name="detail" class="form-control" id="detail"><?php echo $this->input->post('detail'); ?></textarea>
+									<span class="text-danger">
+										<?php echo form_error('detail');?>
+									</span>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<label for="tags" class="control-label">
+									<span class="text-danger">*</span>Tags</label>
+								<div class="form-group">
+									<textarea name="tags" class="form-control" id="tags"><?php echo $this->input->post('tags'); ?></textarea>
+									<span class="text-danger">
+										<?php echo form_error('tags');?>
+									</span>
+								</div>
+							</div>
+							
 						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="added_on" class="control-label"><span class="text-danger">*</span>Added On</label>
-						<div class="form-group">
-							<input type="text" name="added_on" placeholder="Make me a date picker rizwan, tell danish isko carbon->now sy time pick krwao" value="<?php echo $this->input->post('added_on'); ?>" class="form-control" id="added_on" />
-							<span class="text-danger"><?php echo form_error('added_on');?></span>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="author" class="control-label"><span class="text-danger">*</span>Author</label>
-						<div class="form-group">
-							<input type="text" name="author" value="<?php echo $this->input->post('author'); ?>" class="form-control" id="author" />
-							<span class="text-danger"><?php echo form_error('author');?></span>
-						</div>
-					</div>
-<!--					<div class="col-md-6">-->
-<!--						<label for="comment_id" class="control-label"><span class="text-danger">*</span>Comment Id</label>-->
-<!--						<div class="form-group">-->
-<!--							<input type="text" name="comment_id" value="--><?php //echo $this->input->post('comment_id'); ?><!--" class="form-control" id="comment_id" />-->
-<!--							<span class="text-danger">--><?php //echo form_error('comment_id');?><!--</span>-->
-<!--						</div>-->
-<!--					</div>-->
-<!--					<div class="col-md-6">-->
-<!--						<label for="excerpt" class="control-label"><span class="text-danger">*</span>Excerpt</label>-->
-<!--						<div class="form-group">-->
-<!--							<textarea name="excerpt" class="form-control" id="excerpt">--><?php //echo $this->input->post('excerpt'); ?><!--</textarea>-->
-<!--							<span class="text-danger">--><?php //echo form_error('excerpt');?><!--</span>-->
-<!--						</div>-->
-<!--					</div>-->
-					<div class="col-md-6">
-						<label for="detail" class="control-label"><span class="text-danger">*</span>Detail</label>
-						<div class="form-group">
-							<textarea name="detail" class="form-control" id="detail"><?php echo $this->input->post('detail'); ?></textarea>
-							<span class="text-danger"><?php echo form_error('detail');?></span>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="tags" class="control-label"><span class="text-danger">*</span>Tags</label>
-						<div class="form-group">
-							<textarea name="tags" class="form-control" id="tags"><?php echo $this->input->post('tags'); ?></textarea>
-							<span class="text-danger"><?php echo form_error('tags');?></span>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="img_path" class="control-label"><span class="text-danger">*</span>Image Path</label>
-						<div class="form-group">
-							<textarea name="img_path" class="form-control" id="img_path"><?php echo $this->input->post('img_path'); ?></textarea>
-							<span class="text-danger"><?php echo form_error('img_path');?></span>
+						<div class="box-footer">
+							<div class="col-md-6 col-md-offset-3">
+								<button type="submit" class="btn btn-block btn-success">
+									<i class="fa fa-check"></i> Save
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
+				<div class="col-sm-1 col-md-2"></div>
+				<div class="col-sm-4"></div>
 			</div>
-          	<div class="box-footer">
-            	<button type="submit" class="btn btn-success">
-            		<i class="fa fa-check"></i> Save
-            	</button>
-          	</div>
-            <?php echo form_close(); ?>
-      	</div>
-    </div>
+
+			<?php echo form_close(); ?>
+		</div>
+	</div>
 </div>
