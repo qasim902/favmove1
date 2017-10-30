82a887d487a2ed0aa8b45097c30f8f5b9fd3aaf4
rizwanayubi <rizwan.ayubi296@gmail.com>
2017-10-30 22:11:04 +0500

    changings in agency page

--- application/views/agency/index.php
+++ application/views/agency/index.php
@@ -4,7 +4,7 @@
             <div class="box-header">
                 <h3 class="box-title">Agency Listing</h3>
             	<div class="box-tools">
-                    <a href="<?php echo site_url('agency/add'); ?>" class="btn btn-success btn-sm">Add</a> 
+                    <a href="<?php echo site_url('agency/add'); ?>" class="btn btn-success btn-sm">Add Agency</a> 
                 </div>
             </div>
             <div class="box-body">
@@ -14,11 +14,11 @@
 						<th>ID</th>
 						<th>Name</th>
 						<th>Agency Logo</th>
-						<th>Fb Link</th>
+						<!-- <th>Fb Link</th>
 						<th>Linkedin Link</th>
 						<th>Twit Link</th>
 						<th>Gplus Link</th>
-						<th>Youtube Link</th>
+						<th>Youtube Link</th> -->
 						<th>Address</th>
 						<th>Actions</th>
                     </tr>
@@ -27,11 +27,11 @@
 						<td><?php echo $a['id']; ?></td>
 						<td><?php echo $a['name']; ?></td>
 						<td><?php echo $a['agency_logo']; ?></td>
-						<td><a href="<?php echo $a['fb_link']; ?>"><?php echo $a['fb_link']; ?></a></td>
+						<!-- <td><a href="<?php echo $a['fb_link']; ?>"><?php echo $a['fb_link']; ?></a></td>
 						<td><a href="<?php echo $a['linkedin_link']; ?>"><?php echo $a['linkedin_link']; ?></a></td>
 						<td><a href="<?php echo $a['twit_link']; ?>"><?php echo $a['twit_link']; ?></a></td>
 						<td><a href="<?php echo $a['gplus_link']; ?>"><?php echo $a['gplus_link']; ?></a></td>
-						<td><a href="<?php echo $a['youtube_link']; ?>"><?php echo $a['youtube_link']; ?></a></td>
+						<td><a href="<?php echo $a['youtube_link']; ?>"><?php echo $a['youtube_link']; ?></a></td> -->
 						<td><?php echo $a['address']; ?></td>
 						<td>
                             <a href="<?php echo site_url('agency/edit/'.$a['id']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
