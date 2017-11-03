1ce57591aab004339adbd5f9aefc40670d052e27
rizwanayubi <rizwan.ayubi296@gmail.com>
2017-11-03 16:51:12 +0500

    search page

--- application/views/frontend/views/agent_search_result.php
+++ application/views/frontend/views/agent_search_result.php
@@ -13,8 +13,8 @@
             </div>
             <div class="col-md-6 col-sm-6 bg_white agnt_lst heading_space">
                 <h5><a href="#">Admiral Property Partnership Ltd, London</a></h5>
-                <span><b>Buying:</b> 020 8012 5765 <small><a href="#"> Local call rate</a></small></span><br>
-                <span><b>Letting:</b> 020 8012 5765 <small> <a href="#">Local call rate</a></small></span>
+                <span><b>Tel:</b> 020 8012 5765 <small>
+                <p><span><b>Address:</b> This area is for address</span></p>
                 <p class="top10 bottom10">If you want to let or sell your property soon, this should APPeal to you. If you're about to let or sell your property - or if you've been trying to for some time - why not APPoint a new letting or sales agent? You've nothing to lose by giving us a go. And our fresh APProach might just work wonders. Call us - Helen Kovari, Diane Parry or Ian Cundy - We think you will APProve of our service. </p>
                 <a class="btn-more" href="#"><i><img alt="arrow" src="resources/img/arrow-yellow.png"></i>
                 <span>Full Profile</span><i><img alt="arrow" src="resources/img/arrow-yellow.png"></i></a>
@@ -29,13 +29,13 @@
         <div class="col-md-12">
             <div class="col-md-3 col-sm-6 heading_space">
                 <div class="agnt_lst_img image">
-                    <img class="img-thumbnail" src="resources/img/agent-media1.jpg" alt="Image">
+                    <img class="img-thumbnail" src="resources/img/logo_sticky.jpg" alt="Image">
                 </div>
             </div>
             <div class="col-md-6 col-sm-6 bg_white agnt_lst heading_space">
                 <h5><a href="#">Admiral Property Partnership Ltd, London</a></h5>
-                <span><b>Buying:</b> 020 8012 5765 <small><a href="#"> Local call rate</a></small></span>
-                <span><b>Letting:</b> 020 8012 5765 <small> <a href="#">Local call rate</a></small></span>
+                <span><b>Tel:</b> 020 8012 5765 <small><a href="#"> Local call rate</a></small></span>
+                <p><span><b>Address:</b> This area is for address</span></p>
                 <p class="top10 bottom10">If you want to let or sell your property soon, this should APPeal to you. If you're about to let or sell your property - or if you've been trying to for some time - why not APPoint a new letting or sales agent? You've nothing to lose by giving us a go. And our fresh APProach might just work wonders. Call us - Helen Kovari, Diane Parry or Ian Cundy - We think you will APProve of our service. </p>
                 <a class="btn-more" href="#"><i><img alt="arrow" src="resources/img/arrow-yellow.png"></i>
                 <span>Full Profile</span><i><img alt="arrow" src="resources/img/arrow-yellow.png"></i></a>
