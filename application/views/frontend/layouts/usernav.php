6d7d3bf108da64189a8ee905fdee8792e514b056
rizwanayubi <rizwan.ayubi296@gmail.com>
2017-11-04 19:08:53 +0500

    user profile

--- application/views/frontend/layouts/usernav.php
+++ application/views/frontend/layouts/usernav.php
@@ -9,7 +9,7 @@
                 <?php }
                 else if($this->session->userdata('userData')['role'] == "user")
                 { ?>
-                	<!-- <li><a href="usprof" <?php if($_view == 'frontend/views/user_profile'){ ?> class="active" <?php }?>><i class="icon-icons230"></i>Profile</a></li> -->
+                	<li><a href="usprof" <?php if($_view == 'frontend/views/user_profile'){ ?> class="active" <?php }?>><i class="icon-icons230"></i>Profile</a></li>
                 <li><a href="ag_submit" <?php if($_view == 'frontend/views/submit_prop'){ ?> class="active" <?php }?> ><i class="icon-icons215"></i> Submit Property</a></li>
                      <li><a href="usmyfavs" <?php if($_view == 'frontend/views/fav_prop'){ ?> class="active" <?php }?>><i class="icon-icons43"></i> Favorite Properties</a></li>
                 <?php }?>
